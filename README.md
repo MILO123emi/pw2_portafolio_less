84593d744d448b01721bcc6f6163deaa4109e5d9
MILO123emi <miloemi@proton.me>
2026-07-06 16:26:13 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pw2_portafolio_less
---
+++ src/assets/data/portafolio.js
@@ -0,0 +1,81 @@
+// Es exactamente el miso codigo que implementamos en clase
+//Pero igual que en la tarea pasada no consegui que el crousel se viera bien 
+//por ahora por lo menos hago el rel, pero para que se mire el coursel tendria poner las 
+//clases y el css
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let miCarusel = new Carousel("caruselPrincipal");
+    miCarusel.init();
+});
+
+class Carousel {
+    constructor(carouselId, tickTimeInSeconds = 3){
+        this.carouselHolder = document.getElementById(carouselId);
+        this.track = this.carouselHolder.querySelector(".track");
+        this.slides = [...this.track.querySelectorAll(".slide")];
+        this.minLimit = 0;
+        this.maxLimit = this.slides.length - 1;
+        this.currentIndex = 0;
+        this.tickTime = tickTimeInSeconds * 1000;
+        this.tickerId = null;
+        this.direction = 1;
+    }
+    init() {
+        // Generar Botones
+        this.generateNavigationUI();
+        this.tick();
+    }
+    tick(){
+        this.tickerId = setTimeout(
+            ()=>{
+                this.moveNext();
+                this.tick();
+            },
+            this.tickTime
+        );
+    }
+    moveNext(){
+        let tmpNewIndex = this.currentIndex + this.direction;
+        if (tmpNewIndex > this.maxLimit) {
+            tmpNewIndex = this.maxLimit - 1;
+            this.direction = -1;
+        }
+        if (tmpNewIndex < this.minLimit) {
+            tmpNewIndex = this.minLimit + 1;
+            this.direction = 1;
+        }
+        this.moveTo(tmpNewIndex);
+    }
+
+    moveTo(newIndex) {
+        this.currentIndex = newIndex;
+        this.track.style.left = `${-100 * this.currentIndex}vw`;
+    }
+
+    generateNavigationUI() {
+        let btnLeft = document.createElement("BUTTON");
+        let btnRight = document.createElement("BUTTON");
+
+        btnLeft.classList.add("carousel-btn");
+        btnLeft.classList.add("btnleft");
+        btnRight.classList.add("carousel-btn");
+        btnRight.classList.add("btnright");
+
+        btnLeft.textContent = "<";
+        btnRight.textContent = ">";
+
+        btnRight.addEventListener("click", (e)=>{
+            e.preventDefault();
+            e.stopPropagation();
+            // Para mover al siguiente
+            alert("Moviendo A");
+            // Tip: se debe cancelar el timeout y
+            // volver a correr el tick;
+        });
+
+        this.carouselHolder.appendChild(btnLeft);
+        this.carouselHolder.appendChild(btnRight);
+    }
+
+
+}
